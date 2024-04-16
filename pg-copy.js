1e43e2f5c702e9ed87b54a8acce4d881ed9a9a61
Rafael Cecchin <contatorafaelcecchin@gmail.com>
2024-04-16 14:40:18 -0300

    feature: permitir adicionar novas tabelas sem apagar todo o banco

--- pg-copy.js
+++ pg-copy.js
@@ -57,6 +57,11 @@ const argv = yargs(hideBin(process.argv))
       type: 'array',
       demandOption: true,
     },
+    'clean': {
+      description: 'Limpar o banco de dados de destino antes da cópia',
+      type: 'boolean',
+      default: false,
+    },
   })
   .argv;
 
@@ -77,11 +82,10 @@ if (!dbDestInfo) {
   process.exit(1);
 }
 
-const pgDumpOptionsSchema = `pg_dump -U ${dbSourceInfo.user} -h ${dbSourceInfo.host} -p 5432 -E LATIN1 -s -c -C -t ${argv.tables.join(' -t ')} -Fp "${dbSourceInfo.name}" > ${path.join(dumpDir, 'schema')}`;
-const pgDumpOptionsData = `pg_dump -U ${dbSourceInfo.user} -h ${dbSourceInfo.host} -p 5432 -E LATIN1 --rows-per-insert=1000 -Fp --column-inserts -a -t ${argv.tables.join(' -t ')} "${dbSourceInfo.name}" > ${path.join(dumpDir, 'data')}`;
+const clean = argv['clean'];
 
-process.env.LC_CTYPE = 'pt_BR.ISO-8859-1';
-process.env.LC_COLLATE = 'pt_BR.ISO-8859-1';
+const pgDumpOptionsSchema = `pg_dump -U ${dbSourceInfo.user} -h ${dbSourceInfo.host} -p 5432 -E LATIN1 -x -O -s ${clean ? '-c -C' : ''} -t ${argv.tables.join(' -t ')} -Fp "${dbSourceInfo.name}" > ${path.join(dumpDir, 'schema')}`;
+const pgDumpOptionsData = `pg_dump -U ${dbSourceInfo.user} -h ${dbSourceInfo.host} -p 5432 -E LATIN1 --rows-per-insert=1000 -Fp --column-inserts -a -t ${argv.tables.join(' -t ')} "${dbSourceInfo.name}" > ${path.join(dumpDir, 'data')}`;
 
 try {
     process.env.PGPASSWORD = dbSourceInfo.password;
@@ -97,14 +101,22 @@ try {
     process.exit(1);
 }
 
-const pgRestoreSchema = [
+let pgRestoreSchema = [];
+if (clean) {
+  // Se informar o --clean, ele apaga o banco de dados e cria novamente
+  pgRestoreSchema = [
     `psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -c "DROP DATABASE IF EXISTS \\"${dbDestInfo.name}\\""`,
-    `psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -c "CREATE DATABASE \\"${dbDestInfo.name}\\""`,
+    `psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -c "CREATE DATABASE \\"${dbDestInfo.name}\\"" IF NOT EXISTS`,
     `psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -c "GRANT CONNECT ON DATABASE \\"${dbDestInfo.name}\\" TO ${dbDestInfo.user};"`,
-    `psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -c "GRANT USAGE ON SCHEMA public TO ${dbDestInfo.user};"`,
-    `psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -d \"${dbDestInfo.name}\" -1 -f ${path.join(dumpDir, 'schema')}`
-];
-
+    `psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -c "GRANT USAGE ON SCHEMA public TO ${dbDestInfo.user};"`
+  ];
+} else {
+  // Se não informar o --clean, ele apaga apenas as tabelas informadas
+  argv.tables.forEach(element => {
+    pgRestoreSchema.push(`psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -d \"${dbDestInfo.name}\" -1 -c "DROP TABLE IF EXISTS \\"${element}\\""`);
+  });
+}
+pgRestoreSchema.push(`psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -d \"${dbDestInfo.name}\" -1 -f ${path.join(dumpDir, 'schema')}`);
 const pgRestoreData = `psql -U ${dbDestInfo.user} -h ${dbDestInfo.host} -p 5432 -d \"${dbDestInfo.name}\" -1 -f ${path.join(dumpDir, 'data')}`;
 
 try {
